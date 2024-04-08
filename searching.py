fc5b9274d826aa41a30074d971a6907505d1638b
Marek Kollár <marek@wd02-0230.wd02-0230a.kn.vutbr.cz>
2024-04-08 12:38:44 +0200

    ano

--- searching.py
+++ searching.py
@@ -1,15 +1,22 @@
+import json
 import os
 
 # get current working directory path
 cwd_path = os.getcwd()
 
 
-# def linear_search(sek, num):
-#
-#     for i in range(len(sek)):
-#         if sek[i] == num:
+def linear_search(sek, num):
+    pos = []
+    vyskyt = 0
+    slovnik = {'positions': 0,  'count':0}
+    for i in range(len(sek)):
+        if sek[i] == num:
+            pos.append(i)
+            vyskyt = vyskyt + 1
+    slovnik["positions"] = pos
+    slovnik["count"] = vyskyt
 
-
+    return slovnik
 
 def read_data(file_name, field):
     """
@@ -21,11 +28,15 @@ def read_data(file_name, field):
 
     file_path = os.path.join(cwd_path, file_name)
     with open(file_name, "r") as file:
-        
+        data = json.load(file)
+    return data[field]
 
 
 
 def main():
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
+    print(linear_search(sequential_data, 8))
     pass
 
 
